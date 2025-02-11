224c9d76059999cab0ef6af99d4a7c4de71f63fe
Matthieu Patou <mpatou@openai.com>
2025-02-11 00:13:20 +0000

    Don't forget to also do grpc

--- pkg/plugin/builtin/grpc_grpc_cpp.go
+++ pkg/plugin/builtin/grpc_grpc_cpp.go
@@ -3,6 +3,7 @@ package builtin
 import (
 	"github.com/bazelbuild/bazel-gazelle/label"
 	"github.com/stackb/rules_proto/pkg/protoc"
+	"path"
 )
 
 func init() {
@@ -17,19 +18,33 @@ func (p *GrpcGrpcCppPlugin) Name() string {
 	return "grpc:grpc:cpp"
 }
 
+// cppGRPCGeneratedFileName is a utility function that returns a function that
+// computes the name of a predicted generated file having the given
+// extension(s) relative to the given dir.
+func cppGRPCGeneratedFileName(reldir string) func(f *protoc.File) []string {
+	return func(f *protoc.File) []string {
+		name := f.Name
+		if reldir != "" {
+			name = path.Join(reldir, name)
+		}
+		return []string{name + ".grpc.pb.cc", name + ".grpc.pb.h"}
+	}
+}
+
 // Configure implements part of the Plugin interface.
 func (p *GrpcGrpcCppPlugin) Configure(ctx *protoc.PluginContext) *protoc.PluginConfiguration {
 	if !protoc.HasServices(ctx.ProtoLibrary.Files()...) {
 		return nil
 	}
+	cppFiles := protoc.FlatMapFiles(
+		cppGRPCGeneratedFileName(ctx.Rel),
+		protoc.HasService,
+		ctx.ProtoLibrary.Files()...,
+	)
 
 	return &protoc.PluginConfiguration{
-		Label: label.New("build_stack_rules_proto", "plugin/grpc/grpc", "protoc-gen-grpc-cpp"),
-		Outputs: protoc.FlatMapFiles(
-			protoc.ImportPrefixRelativeFileNameWithExtensions(ctx.ProtoLibrary.StripImportPrefix(), ctx.Rel, ".grpc.pb.cc", ".grpc.pb.h"),
-			protoc.HasService,
-			ctx.ProtoLibrary.Files()...,
-		),
+		Label:   label.New("build_stack_rules_proto", "plugin/grpc/grpc", "protoc-gen-grpc-cpp"),
+		Outputs: cppFiles,
 		Options: ctx.PluginConfig.GetOptions(),
 	}
 }
